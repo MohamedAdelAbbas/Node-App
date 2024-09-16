1fbce69e1b153bb7b212514506807e6b2640f4ba
mohamedadelabbas <m.3del95@gmail.com>
2024-09-17 01:11:34 +0300

    Connect to MongoDB With name of container NOT IP

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const app = express();
 const DB_USER = 'root';
 const DB_PASSWORD = 'example';
 const DB_PORT = 27017;
-const DB_HOST = '172.19.0.2';
+const DB_HOST = 'mongo';
 const URI = `mongodb://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}`;
 mongoose.connect(URI)
 .then(() => console.log('connect to db ....'))
