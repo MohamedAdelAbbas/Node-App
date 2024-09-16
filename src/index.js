63e747b64b4f3be5b4090455ba421406003c5582
mohamedadelabbas <m.3del95@gmail.com>
2024-09-17 01:02:10 +0300

    Build mongoDB Container

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 //init app
 const PORT = process.env.PORT || 4000;
 const app = express();
+
+// connect DB
+const DB_USER = 'root';
+const DB_PASSWORD = 'example';
+const DB_PORT = 27017;
+const DB_HOST = '172.19.0.2';
+const URI = `mongodb://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}`;
+mongoose.connect(URI)
+.then(() => console.log('connect to db ....'))
+.catch((err) =>console.log('failed to connect to db: ',err));
+
 
 app.get('/',(req, res) => res.send('<h1> Hello Adel ! prod </h1>'));
 
